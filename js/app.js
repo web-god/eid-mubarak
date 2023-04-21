0c0d2342c68ede653ddf432d3d9fe7783b264086
RashidIqbal-mri <rashidiqbal23705@gmail.com>
2023-04-22 03:30:21 +0530

    init

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 particlesJS("particles-js", {
   particles: {
-    number: { value: 400, density: { enable: true, value_area: 800 } },
+    number: { value: 150, density: { enable: true, value_area: 800 } },
     color: { value: "#ffffff" },
     shape: {
       type: "star",
